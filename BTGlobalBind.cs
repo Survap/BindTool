5c58c4675b4949fad514acd778b844f6b806064b
Tsviets <pity_lpz@hotmail.com>
2020-12-08 16:46:10 -0300

    Updated References - Added [Player] Token and forced global permissions.

--- BTGlobalBind.cs
+++ BTGlobalBind.cs
@@ -47,6 +47,8 @@ namespace BindTools
 
 		public void DoCommand(TSPlayer player)
 		{
+			Group currentGroup = player.Group;
+				player.Group = new SuperAdminGroup();
 			try
 			{
 				if (Looping)
@@ -55,10 +57,13 @@ namespace BindTools
 					{ Count[player.Index] = 0; }
 					if (Awaiting)
 					{
-						BindTools.BTPlayers[player.Index].AddCommand(Commands[Count[player.Index]]);
+						BindTools.BTPlayers[player.Index].AddCommand(Commands[Count[player.Index]].Replace("[Player]", player.Name));
 						player.SendInfoMessage("Command {0} added in queue! Use '{1}bindwait' or '{1}bw' to see current awaiting command.", Commands[Count[player.Index]], TShock.Config.CommandSpecifier);
+						player.Group = currentGroup;
 					}
-					else { TShockAPI.Commands.HandleCommand(player, Commands[Count[player.Index]]); }
+					else { TShockAPI.Commands.HandleCommand(player, Commands[Count[player.Index]].Replace("[Player]", player.Name)); 
+					player.Group = currentGroup;
+					}
 					Count[player.Index]++;
 				}
 				else
@@ -67,10 +72,13 @@ namespace BindTools
 					{
 						if (Awaiting)
 						{
-							BindTools.BTPlayers[player.Index].AddCommand(cmd);
+							BindTools.BTPlayers[player.Index].AddCommand(cmd.Replace("[Player]", player.Name));
 							player.SendInfoMessage("Command {0} added in queue! Use '{1}bindwait' or '{1}bw' to see current awaiting command.", Commands[Count[player.Index]], TShock.Config.CommandSpecifier);
+							player.Group = currentGroup;
 						}
-						else { TShockAPI.Commands.HandleCommand(player, cmd); }
+						else { TShockAPI.Commands.HandleCommand(player, cmd.Replace("[Player]", player.Name));
+						player.Group = currentGroup;
+						}
 					}
 				}
 			}
